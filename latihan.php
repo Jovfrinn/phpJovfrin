9565f09aeadf63e925dfd3939692b35938a9db28
damararyaaa <damararyaarya905@gmail.com>
2023-10-27 08:49:35 +0700

    latihan data dami dan database

--- latihan.php
+++ latihan.php
@@ -1,45 +1,48 @@
 <?php
 
-$siswa = [
-  [ 
-    "nis" => "0225744", 
-    "nama" => "abia", 
-    "kelas" => "11 PPLG 4", 
-    "no_telp" => "0835386755" ,
-    "img" => "abia.jpeg"
-  ],
+// $siswa = [
+//   [ 
+//     "nis" => "0225744", 
+//     "nama" => "abia", 
+//     "kelas" => "11 PPLG 4", 
+//     "no_telp" => "0835386755" ,
+//     "img" => "abia.jpeg"
+//   ],
 
-  [
-    "nis" => "022757540", 
-    "nama" => "agisna", 
-    "kelas" => "11 PPLG 5", 
-    "no_telp" =>  "08353543586" ,
-    "img" => "agisna.jpeg"
-  ],
-  [
-    "nis" => "02275675740", 
-    "nama" => "amanda", 
-    "kelas" => "11 PPLG 6", 
-    "no_telp" =>  "0835536426286",
-    "img" => "amanda.jpeg"
-  ],
-  [
-    "nis" => "02275534220", 
-    "nama" => "rehan", 
-    "kelas" => "11 PPLG 7", 
-    "no_telp" =>  "0835503286" ,
-    "img" => "rehan.jpeg"
-  ],
-  [
-    "nis" => "022750694", 
-    "nama" => "ripaldi", 
-    "kelas" => "11 PPLG 6", 
-    "no_telp" =>  "0835503486",
-     "img" => "ripaldi.jpeg"
-  ],
-];
+//   [
+//     "nis" => "022757540", 
+//     "nama" => "agisna", 
+//     "kelas" => "11 PPLG 5", 
+//     "no_telp" =>  "08353543586" ,
+//     "img" => "agisna.jpeg"
+//   ],
+//   [
+//     "nis" => "02275675740", 
+//     "nama" => "amanda", 
+//     "kelas" => "11 PPLG 6", 
+//     "no_telp" =>  "0835536426286",
+//     "img" => "amanda.jpeg"
+//   ],
+//   [
+//     "nis" => "02275534220", 
+//     "nama" => "rehan", 
+//     "kelas" => "11 PPLG 7", 
+//     "no_telp" =>  "0835503286" ,
+//     "img" => "rehan.jpeg"
+//   ],
+//   [
+//     "nis" => "022750694", 
+//     "nama" => "ripaldi", 
+//     "kelas" => "11 PPLG 6", 
+//     "no_telp" =>  "0835503486",
+//      "img" => "ripaldi.jpeg"
+//   ],
+// ];
+
+$koneksi = mysqli_connect("localhost","root","","siswa11pplg2");
 
 
+$simpan = mysqli_query($koneksi,"")
 
 // var_dump($siswa);
 
--- latihan2.php
+++ latihan2.php
@@ -1,24 +1,56 @@
+<?php
+$koneksi = mysqli_connect('localhost','root','','siswa11pplg2');
+
+$result = mysqli_query($koneksi,"SELECT * FROM siswa");
+
+//untuk mengambil dari kotak besar (fetch)
+// $swa = mysqli_fetch_row($result);
+// $swa = mysqli_fetch_array($result);
+// $swa = mysqli_fetch_object($result);
+
+// var_dump($swa);
+
+
+
+
+
+
+
+
+
+
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        ul li {
+            font-family: Arial, Helvetica, sans-serif;
+            font-size: 15px;
+        }
+    </style>
 </head>
 <body>
     <ul>
-       
+    <?php $no = 1; ?>
+       <?php while($swa = mysqli_fetch_assoc($result)){  ?>
               <li>
-               <h3> Nis: <?php echo $_GET["nis"];  ?></h3>
-               <h3> Nama: <?php echo $_GET["nama"];  ?></h3>
-               <h3> Kelas: <?php echo $_GET["kelas"];  ?></h3>
-               <h3> No Telp: <?php echo $_GET["no_telp"];  ?></h3>
-               <h3> Gambar: <img src="img/<?php echo $_GET["img"];  ?>" width="500px"> </h3>
+                <h3> No: <?= $no++; ?></h3>
+               <h3> Nis: <?php echo $swa["nis"];  ?></h3>
+               <h3> Nama: <?php echo $swa["nama"];  ?></h3>
+               <h3> Kelas: <?php echo $swa["kelas"];  ?></h3>
+               <h3> No Telp: <?php echo $swa["noHP"];  ?></h3>
+             
+     
               
               </li>
 
 
-    
+    <?php } ?>
     </ul>
 
 
