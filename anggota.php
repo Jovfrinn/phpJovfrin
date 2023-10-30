b704161f19da7f8bae27646bfd3f60db089b5e0f
Joiner <alexanderyousuke@gmail.com>
2023-10-30 21:14:15 +0700

    login terbaru

--- anggota.php
+++ anggota.php
@@ -1,5 +1,6 @@
 <?php 
 session_start();
+ob_start();
 if(!isset($_SESSION['session_username'])){
   header("location:login.php");
   exit();
@@ -7,4 +8,23 @@ if(!isset($_SESSION['session_username'])){
 
 print_r($_SESSION);
 print_r($_COOKIE);
+
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+<br><br>
+<form action="logout.php">
+<button type="submit">logout</button>
+</form>
+
+<script>alert("Anda telah Login");</script>
+</body>
+</html>
+
